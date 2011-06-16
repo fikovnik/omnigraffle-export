e61b9998b44fde6749ca60d8c8e00eeb51d2407d
Filip Krikava <krikava@gmail.com>
2011-06-16 10:53:59 +0200

    Version 1.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup (
     name = "omnigraffle-export",
-    version = "1.3",
+    version = "1.3.1",
     packages = find_packages('src'),
     package_dir = {'':'src'},
     install_requires = ['appscript'],
