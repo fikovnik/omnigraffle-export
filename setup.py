00a9b6518e0d8a4dbef31a972abb659a60b19a5e
Filip Krikava <krikava@gmail.com>
2014-03-03 11:04:31 +0100

    1.6 version

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ def read(fname):
 
 setup (
     name = "omnigraffle-export",
-    version = "1.5.1",
+    version = "1.6",
     packages = find_packages(exclude='tests'),
-    install_requires = ['appscript'],
+    install_requires = ['appscript','pyobjc'],
     author = "Filip Krikava",
     author_email = "krikava@gmail.com",
     description = "A command line utility that exports omnigraffle canvases files into various formats.",
@@ -21,11 +21,11 @@ setup (
     url = "https://github.com/fikovnik/omnigraffle-export",
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         'Operating System :: MacOS :: MacOS X',
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Utilities"
     ],
     entry_points = {
