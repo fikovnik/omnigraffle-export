c84da114ca55f7ad721c1fb9601a5d632948c407
Filip Krikava <krikava@gmail.com>
2015-03-12 10:21:55 +0100

    New release 1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup (
     name = "omnigraffle-export",
-    version = "1.6",
+    version = "1.7",
     packages = find_packages(exclude='tests'),
     install_requires = ['appscript','pyobjc'],
     author = "Filip Krikava",
